d6bfaec73dffa9c038a067dd6c564c46ffb80dad
Waqar Akram <waqarakrambhutta@gmail.com>
2022-10-09 20:08:11 +0500

    arithmatic operators in python

---
+++ arithmatic_operator.py
@@ -0,0 +1,9 @@
+def arthop(x,y):
+    print(x+y)
+    print(x-y)
+    print(x*y)
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    arthop(a,b)
--- test.py
+++ test.py
@@ -1,20 +1,4 @@
-import math
-import os
-import random
-import re
-import sys
-
-def intgr(n):
-    if n%2==1:
-        print("Werid")
-    elif n%2==1 and n in range(2,5):
-        print("Not Weird")
-    elif n%2==1 and n in range(6,20):
-        print("Weird")
-    elif n%2==0 and n>20:
-        print("Not Weird")
-
-
-if __name__ == '__main__':
-    n = int(input().strip())
-    intgr(n)
+a=3
+b=5
+# c=a/b
+print(a//b)
